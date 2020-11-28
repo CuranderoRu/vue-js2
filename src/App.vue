fc66cee528a9903b4e9342377d9c9c44df0aeacc
Sergei Mono <sergei.ogarkov@sogarkov.ru>
2020-11-29 00:10:24 +0300

    vue project initial setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <div class="greeting">Hello {{ greeting }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            greeting: 'world'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .greeting {
+        color: darkred;
+    }
+</style>
